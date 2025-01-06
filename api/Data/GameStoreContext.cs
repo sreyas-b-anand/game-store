d568a1603188bacbe61ab5d1d4c35814c1107af6
sreyas <sreyas1045@gmail.com>
2025-01-06 22:07:12 +0530

    new commit

--- api/Data/GameStoreContext.cs
+++ api/Data/GameStoreContext.cs
@@ -6,7 +6,7 @@ namespace api.Data;
 
 public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
 {
-    public DbSet<Game> Games  => Set<Game>();
+    public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genre => Set<Genre>();
 
--- api/Endpoints/GamesEndpoints.cs
+++ api/Endpoints/GamesEndpoints.cs
@@ -9,7 +9,7 @@ public static class GamesEndpoints
 {
     const string GamesURLname = "GetGameById";
 
-    private  static List<GameDto> games = [
+    private static List<GameDto> games = [
         new GameDto(1, "Super Mario Bros.", "Platformer", 29.99m, new DateOnly(1985, 9, 13)),
         new GameDto(2, "The Legend of Zelda", "Action-adventure", 49.99m, new DateOnly(1986, 2, 21)),
         new GameDto(3, "Minecraft", "Sandbox", 19.99m, new DateOnly(2011, 11, 18)),
@@ -17,7 +17,7 @@ public static class GamesEndpoints
         new GameDto(5, "Half-Life 2", "First-person shooter", 9.99m, new DateOnly(2004, 11, 16))
     ];
 
-    
+
     public static RouteGroupBuilder MapEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("games");
@@ -32,18 +32,19 @@ public static class GamesEndpoints
         }).WithName("GetGameById");
 
         //POST 
-        group.MapPost("/", (CreateGameDto newgame , GameStoreContext dbContext) =>
+        group.MapPost("/", (CreateGameDto newgame, GameStoreContext dbContext) =>
         {
-           
-            Game game = new(){
+
+            Game game = new()
+            {
                 Name = newgame.Name,
                 //Genre = dbContext.Genres.Find(newgame.GenreId),
                 GenreId = newgame.GenreId,
                 Price = newgame.Price,
                 ReleaseDate = newgame.ReleaseDate
-        };
-        dbContext.Games.Add(game);
-        dbContext.SaveChanges();
+            };
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
             return Results.Json(game);
         });
 

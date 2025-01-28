bcdd5e4dc28766dc510ed92052c4831ef8d1bfe4
sreyas <sreyas1045@gmail.com>
2025-01-28 21:15:38 +0530

    new commit

--- api/Entities/Game.cs
+++ api/Entities/Game.cs
@@ -8,10 +8,10 @@ public class Game
     public required string Name { get; set; }
     public int GenreId { get; set; }
 
-    public  Genre? Genre { get; set; }
+    public Genre? Genre { get; set; }
     public decimal Price { get; set; }
 
     public DateOnly ReleaseDate { get; set; }
 
-    
+
 }
